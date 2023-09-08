9259c5ce4a3fac7b82fb336721fcf2c15486811d
Olaide <olaideadeleke25@gmail.com>
2023-09-08 22:41:42 +0100

    Variable, if_else_loops

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ *main - Entry Point
+ *
+ *Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i;
+	char c;
+	for (i = 48; i < 58; i++)
+	{
+		putchar(i);
+	}
+	for (c = 'a'; c < 'g'; c++)
+	{
+		putchar(c);
+	}
+	putchar('\n');
+	return (0);
+}

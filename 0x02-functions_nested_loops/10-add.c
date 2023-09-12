84d0d980e34627a30a7c74b55c872452fe2d5e91
Olaide <olaideadeleke25@gmail.com>
2023-09-12 17:33:11 +0100

    Functions and nested loops

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ *add - add two numbers
+ *Return: Always result
+ */
+int add(int a, int b)
+{
+	int result;
+
+	result = a + b;
+	return result;
+}
---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ *
+ */
+void times_table(void)
+{
+	int i, n;
+
+	for (i = 0; i <= 9; i++)
+	{
+		
+	}
+}

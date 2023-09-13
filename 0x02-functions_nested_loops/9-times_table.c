bde66c755c183dc63d6ce1ad26fde2506f472c75
Olaide <olaideadeleke25@gmail.com>
2023-09-13 15:11:50 +0100

    Functions and nested loops

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -1,14 +1,43 @@
 #include "main.h"
 #include <stdio.h>
 /**
+ *times_table - 9times table
  *
+ *Return: Always mul
  */
 void times_table(void)
 {
-	int i, n;
+	int i, j, mul, rem, res;
 
 	for (i = 0; i <= 9; i++)
 	{
-		
+		for (j = 0; j <= 9; j++)
+		{
+			mul = i * j;
+
+			if (mul > 9)
+			{
+				rem = mul % 10;
+				res = (mul - rem) / 10;
+
+				putchar(44);
+				putchar(32);
+				putchar(res + '0');
+				putchar(rem + '0');
+			}
+			else
+			{
+				if (j != 0)
+				{
+					putchar(44);
+					putchar(32);
+					putchar(32);
+				}
+
+				putchar(mul + '0');
+			}
+		}
+
+		putchar('\n');
 	}
 }

55e5196908cb59664ea4c762edc43808117e0eff
Olaide <olaideadeleke25@gmail.com>
2023-09-14 14:52:46 +0100

    More functions and nested loops

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ *mul - products of two integers
+ *@a: first integer
+ *@b: second integer
+ *Return: Always products of the integers
+ */
+int mul(int a, int b)
+{
+	int prod;
+
+	prod = a * b;
+	return (prod);
+}

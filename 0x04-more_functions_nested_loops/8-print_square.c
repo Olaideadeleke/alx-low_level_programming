daf02a9fb7bbf9ccc31bb16ef22a4f34ed83af80
Olaide <olaideadeleke25@gmail.com>
2023-09-14 18:06:13 +0100

    More functions and nested ;oops

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -8,19 +8,20 @@ void print_square(int size)
 {
 	int i, j;
 
-	for (i = 0; i < size; i++)
+	if (size > 0)
 	{
-		if (size > 0)
+		for (i = 0; i < size; i++)
 		{
 			for (j = 0; j < size; j++)
 			{
 				_putchar('#');
+
 			}
-		}
-		else
-		{
 			_putchar('\n');
 		}
+	}
+	else
+	{
 		_putchar('\n');
 	}
 }

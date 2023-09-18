f553588a07160e87f7f35dfd6c913dc8f03f2c62
Olaide <olaideadeleke25@gmail.com>
2023-09-19 00:50:23 +0100

    Pointers, arrays and strings

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,12 @@
+#include "main.h"
+#include <stdio.h>
+#include <string.h>
+/**
+ *_strlen - Count the charaters in a string
+ *@s: string name
+ *Returns: an int
+ */
+int _strlen(char *s)
+{
+	return (strlen(s));
+}

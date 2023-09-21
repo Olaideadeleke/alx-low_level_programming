9bf3678dc7796690ab1cec20d6304e558d038da6
Olaide <olaideadeleke25@gmail.com>
2023-09-21 10:25:11 +0100

    Pointer, arrays and strings

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -9,13 +9,17 @@
  */
 int _strcmp(char *s1, char *s2)
 {
-	int res = strcmp(s1, s2);
+	while (*s1 != '\0' && *s1 == *s2)
+	{
+		s1++;
+		s2++;
+	}
 
-	if (res < 0)
+	if (s1 < s2)
 	{
 		return (-15);
 	}
-	else if (res > 0)
+	else if (s1 > s2)
 	{
 		return (15);
 	}
